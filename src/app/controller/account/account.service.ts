68e475da80c021a9196dcde3250d05f7ae9d01ee
Jacky <13690913046@163.com>
2019-04-12 20:56:45 +0800

    Add remove old header img

--- src/app/controller/account/account.service.ts
+++ src/app/controller/account/account.service.ts
@@ -2,6 +2,7 @@ import { Injectable, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../entitys/user.entity';
+import * as fs from 'fs';
 
 @Injectable()
 export class AccountService {
@@ -18,12 +19,17 @@ export class AccountService {
     const res = await this.accountRepository.findOne({userId: param.userId});
     res.nickName = param.nickName ? param.nickName : res.nickName;
     res.personalProfile = param.personalProfile ? param.personalProfile : res.personalProfile;
-    res.headerIcon = param.headerIcon;
+    if (param.headerIcon) {
+      if (res.headerIcon) {
+        fs.unlinkSync(`./${res.headerIcon}`);
+      }
+      res.headerIcon = param.headerIcon;
+    }
     await this.accountRepository.save(res);
     const msg = {
       code: 200,
       message: '修改成功！',
-    }
+    };
     return msg;
 }
 }
