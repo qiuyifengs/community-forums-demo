db12cec2f9cbfe92ea3320c0b27e49e568abd045
Jacky <13690913046@163.com>
2019-04-28 19:43:45 +0800

    Commit code

--- src/app/entitys/labelType.entity.ts
+++ src/app/entitys/labelType.entity.ts
@@ -5,9 +5,13 @@ import { LabelList } from './labelList.entity';
 export class LabelType {
   @PrimaryGeneratedColumn() serialNum: number;
 
-  @Column({ length: 20 }) typeName: string;
+  @Column({ length: 20 }) typeNameEn: string;
+
+  @Column({ length: 20 }) typeNameCn: string;
 
   @Column() typeId: string;
+
+  @Column({ default: 'cn' }) langue: string;
 
   @OneToMany(type => LabelList, children => children.labelList)
     labelArr: LabelList[];
