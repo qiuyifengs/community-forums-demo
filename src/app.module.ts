28aeac4efdb798be12613276acb503232ad3befa
Jacky <13690913046@163.com>
2019-04-29 16:17:27 +0800

    Commit code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,34 @@ import { Module } from '@nestjs/common';
 import { CorsMiddleware } from '@/bing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import modules from './app';
+import { ArticleDetail, ChildrenComments, CommentsList, Focus, Forum, LabelList, LabelType, Menu, MyCollectionList, PostList, User } from './app/entitys';
+// const ormconfig = require('../ormconfig.json');
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot(), ...modules,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '123456',
+            database: 'db_data',
+            entities: [
+                ArticleDetail,
+                ChildrenComments,
+                CommentsList,
+                Focus,
+                Forum,
+                LabelList,
+                LabelType,
+                Menu,
+                MyCollectionList,
+                PostList,
+                User
+            ],
+            synchronize: true,
+
+        }), ...modules,
     ],
 })
 
