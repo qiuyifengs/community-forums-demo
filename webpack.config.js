9932bf616809b380f68c44e9859b9534de045213
Jacky <13690913046@163.com>
2019-05-06 16:19:31 +0800

    Commit code

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,21 @@ const glob = require('glob');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+/**
+ * ejs 模块打包
+ * @param {*} globPath 
+ */
 function getEntry(globPath) {
   var entries = {},
       basename, tmp;
 
   glob.sync(globPath).forEach(function (entry) {
-      basename = path.basename(entry, path.extname(entry));
+      // basename = path.basename(entry, path.extname(entry));
       tmp = entry.split('/').splice(-3);
+      
       // 过滤掉本模块的 ejs 的入口文件
       if (path.extname(entry).indexOf('.ejs') > -1 || path.extname(entry).indexOf('.js') > -1) {
-          entries[basename] = entry
+          entries[entry] = entry;
       }
   });
   return entries;
@@ -36,6 +41,27 @@ const htmlConfig = () => {
   }
   return config;
 }
+// const CssTpl = getCssEntry('./src/libs/verdors.js')
+// function getCssEntry(globPath) {
+//   var entries = {};
+//   console.log(globPath)
+//   glob.sync(globPath).forEach(function (entry) {
+//       // 过滤掉本模块的 ejs 的入口文件
+//       if (path.extname(entry).indexOf('.css') > -1) {
+//           entries[entry] = entry;
+//       }
+//   });
+//   return entries;
+// }
+// const cssConfig = () => {
+//   let config = []
+//   for (let attr in CssTpl) {
+//     const attrPath = CssTpl[attr].replace('./src/', '')
+//     console.log(2, attrPath)
+//         config.push(new ExtractTextPlugin(`./dist/${attrPath}`))
+//   }
+//   return config;
+// }
 /**
  * 根目录
  * @param {*} subdir 子目录
@@ -45,7 +71,7 @@ function srcPath(subdir) {
 }
 
 module.exports = {
-  entry: ['webpack/hot/poll?1000', './src/main.ts'],
+  entry: ['webpack/hot/poll?1000', './src/main.ts', './src/libs/verdors.js'],
   watch: true,
   target: 'node',
   externals: [
@@ -62,7 +88,7 @@ module.exports = {
       },
       {
 				test: /\.ejs$/,
-        loader: 'ejs-loader',
+        loader: 'underscore-template-loader',
         query: {
           variable: 'data',
           interpolate : '\\{\\{(.+?)\\}\\}',
@@ -71,22 +97,41 @@ module.exports = {
       },
       {
         test: /\.css$/,
-				//配置css的抽取器、加载器。'-loader'可以省去
-				loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-      },
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                {
+                    loader: 'css-loader'
+                }
+            ]
+        })
+    },
+      {
+				//文件加载器，处理文件静态资源
+				test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]'
+        }
+			},
+      {
+				//图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
+				//如下配置，将小于8192byte的图片转成base64码
+				test: /\.(png|jpg|gif|svg)$/,
+				loader: 'url-loader?limit=8192&name=./img/[name][hash].[ext]'
+			}
     ],
   },
   mode: "development",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.ejs', 'css'],
+    extensions: ['.tsx', '.ts', '.js', '.ejs'],
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('libs/style/[name]/[name].css'),
-
+    new ExtractTextPlugin('/libs/style/[name].css'),
   ].concat(htmlConfig()),
   output: {
     path: path.join(__dirname, 'dist'),
