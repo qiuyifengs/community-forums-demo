1d0fc2517cb755bc7b29b0df68c0ccd4911ea175
Jacky <13690913046@163.com>
2019-05-08 17:47:29 +0800

    commmit code

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const jquery = require('jquery');
 
 const glob = require('glob');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -15,18 +16,37 @@ function getEntry(globPath) {
         basename, tmp;
 
     glob.sync(globPath).forEach(function (entry) {
-        // basename = path.basename(entry, path.extname(entry));
+        basename = path.basename(entry, path.extname(entry));
         tmp = entry.split('/').splice(-3);
-        
         // 过滤掉本模块的 ejs 的入口文件
         if (path.extname(entry).indexOf('.ejs') > -1 || path.extname(entry).indexOf('.js') > -1) {
-            entries[entry] = entry;
+            // if (globPath.indexOf('libs') > -1) {
+            //     entries[entry.replace('./src/libs/js', 'common')] = entry
+            // } else {
+                entries[basename] = entry;
+            // }
         }
     });
     return entries;
 }
 const HtmlTpl = getEntry('./src/**/*.ejs')
-const Entry = getEntry('./src/libs/**/*.js')
+const appPath = {app: ['webpack/hot/poll?1000', './src/main.ts']}
+// const commonJs =  getEntry('./src/libs/js/**/*.js')
+const common = { common: [
+    './src/libs/js/jquery-3.3.1.min.js', 
+    './src/views/common/common.js',
+    './src/libs/js/pageTurning.js',
+    './src/libs/js/ghost-ui.js',
+    './src/libs/js/jquery.cookie.js',
+    './src/libs/js/httphijack1.0.0.js',
+    './src/libs/js/waves.min.js',
+    './src/libs/js/layer.js',
+    './src/libs/js/tagsinput.js',
+    './src/libs/js/autosize.js',
+    './src/libs/js/dropload/dropload.min.js',
+    './src/libs/js/dropload/zepto.min.js'
+]}
+const Entry = Object.assign(getEntry('./src/views/**/*.js'), appPath, common)
 const htmlConfig = () => {
     let config = []
     for (let attr in HtmlTpl) {
@@ -36,7 +56,7 @@ const htmlConfig = () => {
                 filename: `./dist/${attrPath}`,
                 template: HtmlTpl[attr],
                 inject: "head",
-                chunks: ['app',HtmlTpl[attr].split('/')[3]], // 预览模块js独立打包
+                chunks: ['common', HtmlTpl[attr].split('/')[HtmlTpl[attr].split('/').length-1].replace('.ejs', '')], // 预览模块js独立打包
                 chunksSortMode: 'manual',
                 minify: true
             })
@@ -45,25 +65,25 @@ const htmlConfig = () => {
     }
     return config;
 }
-const CssTpl = getCssEntry('./src/libs/style/**/*.css')
-function getCssEntry(globPath) {
-    var entries = {};
-    console.log(globPath)
-    glob.sync(globPath).forEach(function (entry) {
-        if (path.extname(entry).indexOf('.css') > -1) {
-            entries[entry] = entry;
-        }
-    });
-    return entries;
-}
-const cssConfig = () => {
-    let config = []
-    for (let attr in CssTpl) {
-        const attrPath = CssTpl[attr].replace('./src/', '')
-        config.push(new ExtractTextPlugin(`./dist/${attrPath}`))
-    }
-    return config;
-}
+// const CssTpl = getCssEntry('./src/libs/style/**/*.css')
+// function getCssEntry(globPath) {
+//     var entries = {};
+//     console.log(globPath)
+//     glob.sync(globPath).forEach(function (entry) {
+//         if (path.extname(entry).indexOf('.css') > -1) {
+//             entries[entry] = entry;
+//         }
+//     });
+//     return entries;
+// }
+// const cssConfig = () => {
+//     let config = []
+//     for (let attr in CssTpl) {
+//         const attrPath = CssTpl[attr].replace('./src/', '')
+//         config.push(new ExtractTextPlugin(`./dist/${attrPath}`))
+//     }
+//     return config;
+// }
 /**
  * 根目录
  * @param {*} subdir 子目录
@@ -73,11 +93,7 @@ function resolve(subdir) {
 }
 module.exports = {
     // entry: ['webpack/hot/poll?1000', './src/main.ts'],
-    entry: {
-        app: ['webpack/hot/poll?1000', './src/main.ts'],
-        demo: path.resolve(__dirname, 'src/views/demo/demo.js'),
-        demo1: path.resolve(__dirname, 'src/views/demo1/demo1.js'),
-    },
+    entry: Entry,
     watch: true,
     target: 'node',
     externals: [
@@ -129,23 +145,28 @@ module.exports = {
                     ]
                 })
             },
-            // {
-            // 	//文件加载器，处理文件静态资源
-            // 	test: /\.(png|jpg|gif|svg)$/,
-            //   loader: 'file-loader',
-            //   options: {
-            //     name: 'images/[name].[ext]'
-            //   }
-            // },
+            {
+            	//文件加载器，处理文件静态资源
+            	test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader?name=./dist/libs/images/[name].[ext]',
+                
+            },
             {
               //图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
               //如下配置，将小于8192byte的图片转成base64码
               test: /\.(png|jpg|gif|svg)$/,
               loader: 'url-loader?limit=8192&name=./dist/libs/images/[name].[ext]'
-            }
+            },
+            {
+                loader: 'image-webpack-loader',// 压缩图片
+                options: {
+                  bypassOnDebug: true,
+                }
+            },
         ],
     },
     mode: "development",
+    // mode: "production",
     resolve: {
         extensions: ['.tsx', '.ts', '.js', '.ejs'],
         alias: {
@@ -154,22 +175,19 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new ExtractTextPlugin('./dist/libs/style/[name].css'),
+        new ExtractTextPlugin('dist/libs/style/[name].css'),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery',
             'window.$': 'jquery',
           }),
-        //   new HtmlWebpackPlugin({
-        //     filename: `./dist/demo.html`,
-        //     template: './src/views/demo/demo.ejs',
-        //     // inject: "head",
-        //     // minify: true
-        // })
+          new webpack.ProvidePlugin({
+       $:"jquery"
+     }),
     ].concat(htmlConfig()),
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name].js' == 'demo.js' ? 'build.js': 'js/[name].[hash].js',
+        filename: '[name].js' == 'demo.js' ? 'build.js': 'dist/js/[name].[hash].js',
     },
 };
